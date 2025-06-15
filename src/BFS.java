3039257a5b23d6af73e29c3f989e0deaf39316cc
Dani7867 <dsheikh672@gmail.com>
2025-06-15 22:07:56 +0530

    Breadth First Traversal - Desperate

---
+++ src/BFS.java
@@ -0,0 +1,32 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BFS {
+
+    public static void BFSTravel(int s ,AdjacencyMapGraph graph,int n ){
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] isVisited = new boolean[n];
+
+        queue.offer(s);
+        isVisited[s] = true;
+
+        while (!queue.isEmpty()){
+            int curr = queue.poll();
+            System.out.print(curr+" ");
+            for(int neighbour : graph.getNeighbors(curr)){
+                if(!isVisited[neighbour]){
+                    queue.offer(neighbour);
+                    isVisited[neighbour] = true;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        AdjacencyMapGraph graph = new AdjacencyMapGraph(5,false);
+        int[][] edges = {{0,1},{1,4},{1,2},{2,3}};
+        graph.graphFromEdges(edges);
+        graph.printGraph();
+        System.out.println("BFS Traversal:");
+        BFSTravel(0,graph,5);
+    }
+}
